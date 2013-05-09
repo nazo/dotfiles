ecc063d253e79a46f73867e2dee9645a59b146ea
takuya sato <takuya0219@gmail.com>
2013-05-09 17:33:28 +0900

    add homebrew init script

---
+++ brew_install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+brew doctor
+brew update
+
+brew install jq
+brew install ctags
+brew install git
+brew install git-now
+brew install gettext
+brew install jsl
+brew install memcached
+brew install tmux
+brew install wget
+brew install tig
+brew install zsh
+brew install mobile-shell
+brew install phpenv
+
