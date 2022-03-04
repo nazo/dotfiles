fdb5a9597fb0b0a8d670362e5f11d494888be9e7
nazo <nazo@nazo.dev>
2022-03-04 20:55:20 +0900

    hyper

--- mac/packages.sh
+++ mac/packages.sh
@@ -8,4 +8,6 @@ brew update
 brew upgrade
 brew install peco the_silver_searcher ctags python fzf git zsh tmux fzy direnv wget vim git-delta gh act certbot foreman heroku gomplate mint pwgen qcachegrind terraform hey asdf ack awslogs
 brew tap thoughtbot/formulae
+brew install hyper --cask
 brew install rcm
+
